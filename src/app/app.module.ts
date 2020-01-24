9503b3060c5871da893844d726fc764ca2ed6cd0
KadarLuis <kadarluis@hotmail.com>
2020-01-24 10:42:59 -0500

    clear module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,20 @@ import { NgModule } from '@angular/core';
 
 /* ROUTES */
 import { AppRoutingModule } from './app-routing.module';
+import { APP_ROUTES } from './app.routes';
 
 /* MODULE */
 import { PagesModule } from './pages/pages.module';
 
+/* COMPONENT */
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    /* NopagefoundComponent,
-    DashboardComponent, */
-    // HeaderComponent,
-    // SidebarComponent,
-    // BreadcrumsComponent,
-    /* PagesComponent, */
     RegisterComponent
   ],
   imports: [
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
 import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 
 const appRoutes: Routes = [
-    // {
-    //     path: '',
-    //     component: PagesComponent,
-    //     children: [
-    //         {path: 'dashboard', component: DashboardComponent},
-    //         {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-    //     ]
-    // },
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path: '**', component: NopagefoundComponent}
